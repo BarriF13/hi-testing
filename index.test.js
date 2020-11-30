114e35a7d9d664ff1482ec2c4eb8cfadd10d7178
Barri <info@bdesignstudio.co.uk>
2020-11-30 12:37:27 +0000

    fixing three issues , glob var , code break , test description

--- index.test.js
+++ index.test.js
@@ -1,23 +1,58 @@
 const { forEach, map } = require('./index');
+//Helper Function 
+const test = (desc, fn) => {
+  console.log('----', desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err);
+  }
 
-let sum = 0;
-forEach([1,2,3] , (value) =>{
-  sum += value;
+};
+//--- second version of test
+test('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
   });
-  if( sum !== 6){
+  if (sum !== 6) {
     throw new Error('Expected summing array to equal 6');
   }
-
-
- const res =  map([1,2,3] , value => {
-  return value * 2;
+});
+//--- first version of test
+// let sum = 0;
+// forEach([1,2,3] , (value) =>{
+//   sum += value;
+//   });
+//   if( sum !== 6){
+//     throw new Error('Expected summing array to equal 6');
+//   }
+//--- second version of test
+test('The map function', () => {
+  const res = map([1, 2, 3], value => {
+    return value * 2;
   })
-  if( res[0] !== 2){
+  if (res[0] !== 2) {
     throw new Error(`Expected to find 2 but we found ${res[0]}`);
   }
-  if( res[1] !== 4){
+  if (res[1] !== 4) {
     throw new Error(`Expected to find 4 but we found${res[1]}`);
   }
-  if( res[2] !== 6){
+  if (res[2] !== 6) {
     throw new Error(`Expected to find 6 but we found${res[2]}`);
   }
+
+});
+//--- first version of test
+//  const res =  map([1,2,3] , value => {
+//   return value * 2;
+//   })
+  // if( res[0] !== 2){
+  //   throw new Error(`Expected to find 2 but we found ${res[0]}`);
+  // }
+  // if( res[1] !== 4){
+  //   throw new Error(`Expected to find 4 but we found${res[1]}`);
+  // }
+  // if( res[2] !== 6){
+  //   throw new Error(`Expected to find 6 but we found${res[2]}`);
+  // }
